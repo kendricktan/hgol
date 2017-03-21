293b630214428d3cc0007e93b9f55ac6801f1d2e
Kendrick Tan <kendricktan0814@gmail.com>
2017-03-20 17:12:13 -0700

    print grids

--- app/Main.hs
+++ app/Main.hs
@@ -18,22 +18,37 @@ windowY = 10
 
 data GridState = GridState { gsX :: Int, gsY :: Int, alive :: Bool } deriving (Show, Generic)
 
-randomBool :: StdGen -> Int -> [Bool]
-randomBool s n = take n $ unfoldr (Just . random) s :: [Bool]
+isNewLine :: GridState -> String
+isNewLine g
+    | nl && end = "|"
+    | nl = "|\n"
+    | otherwise = ""
+    where nl  = (==) windowX (gsX g)
+          end = (==) windowX (gsX g) && (==) windowY (gsY g)
 
-genGridState :: Int -> Int -> [Bool] -> [GridState]
-genGridState maxX maxY bB = [ GridState cx cy cb | (GridState cx cy _, cb) <- zip ig bB ]
-    where ig = [ GridState x y False | x <- [0..maxX], y <- [0..maxY] ]
+randomBool :: StdGen -> [Bool]
+randomBool s = take (windowX * windowY) $ unfoldr (Just . random) s :: [Bool]
+
+genGridState :: [Bool] -> [GridState]
+genGridState bB = [ GridState cx cy cb | (GridState cx cy _, cb) <- zip ig bB ]
+    where ig = [ GridState x y False | y <- [1..windowY], x <- [1..windowX] ]
 
 strGridState :: GridState -> String
 strGridState gs
-    | x         = "x"
-    | otherwise = " "
+    | x         = "x" ++ nl
+    | otherwise = " " ++ nl
     where x = alive gs
+          nl = isNewLine gs
+
+putStrGridState :: [GridState] -> IO ()
+putStrGridState g = do
+    putStrLn $ intercalate "" [ "=" | _ <- [0..windowX*2] ]
+    putStrLn $ intercalate "" [ "|" ++ strGridState cg | cg <- g ]
+    putStrLn $ intercalate "" [ "=" | _ <- [0..windowX*2] ]
 
 main :: IO ()
 main = do
     s <- newStdGen
-    let grids = genGridState windowX windowY (randomBool s (windowX * windowY))
-    print grids
+    let grids = genGridState (randomBool s)
+    putStrGridState grids
 
